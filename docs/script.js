fa7238efde51c5805d51b9d91b490f9f5e8e3710
mathew harvey <mathewharvey@gmail.com>
2023-12-13 00:11:26 +0800

    added data persistance, printing and sharing

--- docs/script.js
+++ docs/script.js
@@ -8,10 +8,14 @@ const firebaseConfig = {
     messagingSenderId: "72962197769",
     appId: "1:72962197769:web:886cc755f32e9b3d6f6bd8",
     measurementId: "G-6E144LFLGX"
-  };
+};
 
-// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+
+const database = firebase.database();
 
 
 let eventCounter = 0;
@@ -101,6 +105,8 @@ function createWeekBoxes(container, totalWeeksLived, totalYears) {
         container.appendChild(yearContainer);
     }
 }
+//make calculate age robust
+
 
 function calculateAge() {
     const inputBirthDate = document.getElementById('birthdate').value;
@@ -118,9 +124,15 @@ function calculateAge() {
         const weeksSinceLastBirthday = Math.ceil(daysSinceLastBirthday / 7);
         totalWeeksLived = age * 52 + weeksSinceLastBirthday;
 
+        if (auth.currentUser) {
+            saveBirthDateToDatabase(auth.currentUser.uid, birthDate);
+        }
+
         createWeekBoxes(document.getElementById('chart-container'), totalWeeksLived, 90);
     }
 }
+
+
 
 $(function () {
     $("#floating-div").draggable({ containment: "window" }).resizable();
@@ -182,8 +194,8 @@ $(function () {
             preferredFormat: "hex",
             showPalette: true,
             palette: [
-                ["#000","#444","#666","#999","#ccc","#eee","#f3f3f3","#fff"],
-                ["#f00","#f90","#ff0","#0f0","#0ff","#00f","#90f","#f0f"]
+                ["#000", "#444", "#666", "#999", "#ccc", "#eee", "#f3f3f3", "#fff"],
+                ["#f00", "#f90", "#ff0", "#0f0", "#0ff", "#00f", "#90f", "#f0f"]
             ],
             change: function (color) {
                 $('#' + eventColorId).val(color.toHexString());
@@ -199,20 +211,34 @@ $(function () {
 
 function addOrUpdateEvent(counter) {
     const name = $('#event-name-' + counter).val();
-    const start = $('#event-start-' + counter).datepicker('getDate');
-    const end = $('#event-end-' + counter).datepicker('getDate');
+    const startStr = $('#event-start-' + counter).val();
+    const endStr = $('#event-end-' + counter).val();
     const color = $('#event-color-' + counter).val();
 
-    const event = lifeEvents.find(e => e.id === 'event-' + counter);
-    if (event) {
-        event.name = name;
-        event.start = start;
-        event.end = end;
-        event.color = color;
+    // Convert strings to Date objects
+    const start = new Date(startStr);
+    const end = new Date(endStr);
+
+    // Find if the event already exists
+    const eventIndex = lifeEvents.findIndex(e => e.id === 'event-' + counter);
+    if (eventIndex !== -1) {
+        // Update existing event
+        lifeEvents[eventIndex] = { id: 'event-' + counter, name, start, end, color };
     } else {
+        // Add new event
         lifeEvents.push({ id: 'event-' + counter, name, start, end, color });
+        eventCounter++;
     }
+
     createWeekBoxes(document.getElementById('chart-container'), totalWeeksLived, 90);
+    if (auth.currentUser) {
+        const firebaseLifeEvents = lifeEvents.map(event => ({
+            ...event,
+            start: event.start.toISOString(),
+            end: event.end.toISOString()
+        }));
+        saveLifeEventsToDatabase(auth.currentUser.uid, firebaseLifeEvents);
+    }
 }
 
 function createCompactEventView(event, counter) {
@@ -260,8 +286,10 @@ function removeEvent(counter) {
     $('#event-details-' + counter).remove();
     $('#compact-event-' + counter).remove();
     lifeEvents = lifeEvents.filter(event => event.id !== 'event-' + counter);
+    if (auth.currentUser) {
+        saveLifeEventsToDatabase(auth.currentUser.uid, lifeEvents);
+    }
 }
-
 function compactPreviousEvents() {
     lifeEvents.forEach((event, index) => {
         if (index < eventCounter) {
@@ -319,8 +347,6 @@ function createEventLabels(container, lifeEvents, birthDate) {
 }
 
 createEventLabels(document.getElementById('chart-container'), lifeEvents, birthDate);
-// Firebase Authentication
-const auth = firebase.auth();
 
 function registerUser(email, password) {
     auth.createUserWithEmailAndPassword(email, password)
@@ -334,17 +360,23 @@ function registerUser(email, password) {
         });
 }
 
-function loginUser(email, password) {
-    auth.signInWithEmailAndPassword(email, password)
-        .then((userCredential) => {
-            // Login successful
-            console.log('User logged in:', userCredential.user);
-            switchToLoggedInState(userCredential.user);
-        })
-        .catch((error) => {
-            console.error('Login failed:', error.message);
-        });
+function loginUser() {
+    const email = document.getElementById('login-email').value;
+    const password = document.getElementById('login-password').value;
+    if (email && password) {
+        auth.signInWithEmailAndPassword(email, password)
+            .then((userCredential) => {
+                console.log('User logged in:', userCredential.user);
+                switchToLoggedInState(userCredential.user);
+            })
+            .catch((error) => {
+                console.error('Login failed:', error.message);
+            });
+    } else {
+        console.error('Email or password is missing');
+    }
 }
+
 
 function logoutUser() {
     auth.signOut().then(() => {
@@ -359,6 +391,9 @@ function logoutUser() {
 auth.onAuthStateChanged((user) => {
     if (user) {
         console.log('Logged in user email:', user.email);
+        loadBirthDateFromDatabase(user.uid); // Load birth date
+        loadLifeEventsFromDatabase(user.uid); // Load life events
+
         switchToLoggedInState(user);
     } else {
         console.log('User is logged out');
@@ -382,11 +417,13 @@ function switchToLoggedOutState() {
 }
 
 function onUserLoggedIn(user) {
-   // Display the user's email in the navigation bar
-   const userEmailDisplay = document.getElementById('user-email');
-   if (userEmailDisplay) {
-    userEmailDisplay.textContent = user.email; // Display the user's email
-}
+    loadLifeEventsFromDatabase(user.uid);
+    loadBirthDateFromDatabase(user.uid);
+    // Display the user's email in the navigation bar
+    const userEmailDisplay = document.getElementById('user-email');
+    if (userEmailDisplay) {
+        userEmailDisplay.textContent = user.email; // Display the user's email
+    }
     document.getElementById('auth-container').style.display = 'none';
     document.getElementById('register-section').style.display = 'none';
     document.getElementById('logout-button').style.display = 'block';
@@ -426,7 +463,8 @@ $('#login-button').click(() => {
         loginUser(email, password);
     } else {
         console.error('Email or password is missing');
-    }});
+    }
+});
 
 $('#register-button').click(() => {
     const email = $('#register-email').val();
@@ -436,13 +474,72 @@ $('#register-button').click(() => {
 
 $('#logout-button').click(logoutUser);
 
-auth.onAuthStateChanged((user) => {
-    if (user) {
-        switchToLoggedInState(user);
-    } else {
-        switchToLoggedOutState();
-    }
-});
 
 
-   
+
+
+function saveLifeEventsToDatabase(userId, lifeEvents) {
+    database.ref('users/' + userId + '/lifeEvents').set(lifeEvents);
+}
+
+function loadLifeEventsFromDatabase(userId) {
+    database.ref('users/' + userId + '/lifeEvents').once('value').then((snapshot) => {
+        const events = snapshot.val();
+        if (events) {
+            lifeEvents = events.map(event => ({
+                    ...event, 
+                    start: new Date(event.start), 
+                    end: new Date(event.end)
+            }));
+            createWeekBoxes(document.getElementById('chart-container'), totalWeeksLived, 90);
+        }
+    });
+}
+
+
+function saveBirthDateToDatabase(userId, birthDate) {
+    database.ref('users/' + userId + '/birthDate').set(birthDate.toISOString());
+}
+
+function loadBirthDateFromDatabase(userId) {
+    database.ref('users/' + userId + '/birthDate').once('value').then((snapshot) => {
+        const birthDateString = snapshot.val();
+        if (birthDateString) {
+            birthDate = new Date(birthDateString);
+            document.getElementById('birthdate').valueAsDate = birthDate;
+            calculateAge();
+        }
+    });
+}
+
+function printContent() {
+    window.print();
+}
+
+function printGrid() {
+    const mainContent = document.getElementById("main-content");
+
+    html2canvas(mainContent, { scale: 2, logging: true, scrollY: -window.scrollY }).then(canvas => {
+        const image = canvas.toDataURL("image/png");
+        const windowContent = '<!DOCTYPE html>' +
+                              '<html>' +
+                              '<head><title>Print</title></head>' +
+                              '<body style="margin: 0; padding: 0; box-sizing: border-box; font-family: Arial;">' +
+                              '<div style="text-align: center; margin: auto; page-break-inside: avoid;">' +
+                                  '<img src="' + image + '" style="width: 100%; max-width: 800px; max-height: 1120px; height: auto;">' +
+                              '</div>' +
+                              '</body>' +
+                              '</html>';
+        const printWin = window.open('', '', 'width=840,height=1189'); // A4 size: 8.27 × 11.69 inches
+        printWin.document.open();
+        printWin.document.write(windowContent);
+        printWin.document.close();
+        printWin.focus();
+        setTimeout(function () {
+            printWin.print();
+            printWin.close();
+        }, 1000);
+    }).catch(error => {
+        console.error('Error generating print image:', error);
+    });
+}
